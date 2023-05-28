49b614b2db9979dd188b73085f992bd6cccce0e3
hcdclove <131356756+hcdclove@users.noreply.github.com>
2023-05-28 07:29:09 -0400

    Version3.0

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const herokuPost = process.env.PORT;
-const localPort = 3000;
+const localPort = 3001;
 
 const app = express();
 
--- public/index.js
+++ public/index.js
@@ -21,3 +21,35 @@ document.getElementById('sixsigma').value = 'sixsigma';
 document.getElementById('enterprisepm').value = 'enterprisepm';
 document.getElementById('schedulingpatent').value = 'schedulingpatent';
 document.getElementById('enterprisewide').value = 'enterprisewide';
+
+function changeBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+function handleClick(x) {
+  var isChecked = x.checked;
+  alert(isChecked);
+  if (isChecked) {
+    // alert($('main').css);
+    $('div:not(#movingbutton)').css('filter', 'invert(100%)');
+    $('img').css('filter', 'invert(100%)');
+    $('*').css('filter', 'invert(100%)');
+    $('main').css('filter', 'invert(100%)');
+    $('div').css('filter', 'invert(100%)');
+    $('nav').css('filter', 'invert(100%)');
+    $('h1').css('filter', 'invert(100%)');
+    $('p').css('filter', 'invert(100%)');
+  } else {
+    alert('else');
+    $('div:not(#movingbutton)').css('filter', 'invert(0%)');
+    $('img').css('filter', 'invert(0%)');
+    $('*').css('filter', 'invert(0%)');
+    $('main').css('filter', 'invert(0%)');
+    $('div').css('filter', 'invert(0%)');
+    $('nav').css('filter', 'invert(0%)');
+    $('h1').css('filter', 'invert(0%)');
+    $('p').css('filter', 'invert(0%)');
+  }
+}
+//   if (isChecked) changeBackgroundColor('black');
+//   else changeBackgroundColor('white');
